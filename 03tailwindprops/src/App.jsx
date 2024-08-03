7b5994ed7b2024935a15e299863500149b5594c4
Stanley Ke <kestanley101@gmail.com>
2024-08-02 21:27:14 -0400

    update

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  let myObj = {
+    name: "Stanley",
+    age: 19,
+    address: {
+      city: "NYC",
+      state: "NY",
+      country: "US",
+    },
+  };
+  let newArr = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
-      <h1 className="text-red-500">blahblabhalbahb</h1>
+      <h1 className="text-3xl bg-green-500 p-2 rounded-md">
+        Vite with tailwind
+      </h1>
+      <Card
+        username={myObj.name}
+        occupation="Student"
+        locale={myObj.address.country}
+        myArr={newArr}
+      />
+      <Card />
     </>
   );
 }
